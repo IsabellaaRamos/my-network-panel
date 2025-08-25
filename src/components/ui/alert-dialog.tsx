e2e66c1a88a91e1fd13ccfa5220936d9cd74242f
isabellaaRamos <isabellaramos7@outlook.com>
2025-08-25 10:53:22 -0300

    Corrige e aprimora o componente AlertDialog com novos elementos de cabeçalho e rodapé

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -3,6 +3,7 @@ import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants} from "@/components/ui/button"
+import { rootPropsReducer } from "recharts/types/state/rootPropsSlice"
 
 const AlertDialog = AlertDialogPrimitive.Root
 
@@ -34,6 +35,63 @@ const AlertDialogContent = React.forwardRef<
         <AlertDialogPrimitive.Content
             ref={ref}
             className={cn( 
-                "fixed left-[50%] top-[50%] z- 50 grid w-full max-w-lg translate-x-"
+                "fixed left-[50%] top-[50%] z- 50 grid w-full max-w-lg translate-x-",
+                className
             )}
+            {...props}
+        />
+    </AlertDialogPortal> 
 ))
+AlertDialogContent.displayName = AlertDialogPrimitive.Content.displayName 
+
+const AlertDialogContent.displayName = AlertDialogPrimitive.Content.displayName 
+
+const AlertDialogHeader = ({
+    className,
+    ...props 
+}: React.HTMLAttributes<HTMLDivElement>) => (
+    <div
+        className={cn(
+            "flex flex-col space-y-2 text-center sm:text-left",
+            className
+        )}
+        {...props}
+    />
+
+)
+AlertDialogHeader.displayName = "AlertDialogHeader"
+
+const AlertDialogFooter = ({
+    className,
+    ...rootPropsReducer
+}: React.HTMLAttributes<HTMLDivElement>) => (
+    <div
+       className={cn(
+        "flex flex-col space-y-2 text-center sm:text-left",
+        className 
+       )}
+   {...props}
+  />
+
+ )
+ AlertDialogHeader.displayName = "AlertDialogHeader"
+
+ const AlertDialogFooter = ({
+    className,
+    ...props
+ }: React.HTMLAttributes<HTMLDivElement>) => (
+    <div
+      className={cn(
+        "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
+        className
+      )}
+      {...props}
+      />
+ )
+ AlertDialogFooter.displayName = "AlertDialogFooter"
+
+ 
+ 
+
+
+
